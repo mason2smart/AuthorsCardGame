880e9421747d9564f9e86daae6648c63176d1808
Mason Cole <mason.s.cole@hotmail.com>
2017-09-11 20:02:38 -0400

    Added preview cards to human player selector window

--- Author'sCardGame/src/HumanPlayer.java
+++ Author'sCardGame/src/HumanPlayer.java
@@ -1,11 +1,17 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 
@@ -13,11 +19,13 @@ import javax.swing.UIManager;
 public class HumanPlayer extends AI{
 	JFrame humanFrame;
 	JPanel humanPanel;
+//	JFrame ImgFrame;
 	private JButton select;
 	private JComboBox<String> choices;
 	private int cardToGet;
 	private Boolean cardNotChosen;
-
+	private JLabel img;
+	String card = " ";
 	public HumanPlayer(int x, ArrayList<String> history)
 	{
 		super(x, history);
@@ -31,21 +39,37 @@ public class HumanPlayer extends AI{
 	public void initHumanPanel()
 	{
 		 choices = new JComboBox<String>();
-		 select = new JButton("Submit"); 
+		 select = new JButton("Submit");
+		//ImageIcon AAA =new ImageIcon("cards_jpeg/5D.jpg");
+		img = new JLabel();
+		//System.out.println("AAA Height" + AAA.getIconHeight());
+		//System.out.println("AAA location" + AAA.getDescription());
+		img.setLayout(new BorderLayout());
 
-				humanFrame = new JFrame();		
+		img.setVisible(true);
+				humanFrame = new JFrame();
+			//	ImgFrame = new JFrame();
+			//	ImgFrame.setPreferredSize(new Dimension(500,100));
+			//	ImgFrame.setVisible(true);
+			//	ImgFrame.add(img);
+			//	ImgFrame.pack();
 				humanPanel = new JPanel();
+				humanPanel.setLayout(new FlowLayout());
+				humanPanel.setBackground(Color.black);
 				humanFrame.setVisible(false);
 				humanFrame.setTitle("Human Player");
-				humanPanel.setPreferredSize(new Dimension(300,300));
+				humanPanel.setPreferredSize(new Dimension(450,800));
 				humanPanel.setAlignmentY(TOP_ALIGNMENT);
 				humanPanel.setAlignmentX(RIGHT_ALIGNMENT);
+				img.setVisible(true);
 				humanFrame.add(humanPanel);
-				humanFrame.setResizable(false);
 				humanFrame.setAlwaysOnTop(true);
-				 humanPanel.add(choices);
-				 humanPanel.add(select);
-				 humanFrame.pack();
+				humanFrame.setResizable(false);
+				humanPanel.add(choices);
+				humanPanel.add(select);
+				humanPanel.add(img);
+				humanFrame.pack();
+
 				try//try to get system look for gui interface
 				{
 					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -63,8 +87,7 @@ public class HumanPlayer extends AI{
 	public void initComboChoice(ArrayList<Card> hand, int lim)
 	{
 		cardNotChosen=true;
-		for(int i = 0;i<lim-1;i++)
-		{
+		for(int i = 0;i<lim-1;i++) {
 			choices.addItem(neededCards.get(i).toString());
 		}
 		humanFrame.pack();
@@ -86,7 +109,38 @@ public class HumanPlayer extends AI{
 				}}	);
 			
 			select.setText("Submit");
+		addImage();
 	}
+	public void addImage(){
+		Thread a = new Thread(){
+			public void run(){
+				while(cardNotChosen)
+				{
+					try
+					{
+						String newCard = (neededCards.get(choices.getSelectedIndex()).rank()+neededCards.get(choices.getSelectedIndex()).suit());
+						if(!(newCard.equals(card))){
+							String filename = ("cards_jpeg/" + newCard + ".jpg");
+							System.out.println(filename);
+							ImageIcon imgIco = new ImageIcon(filename);
+							imgIco.setImage(imgIco.getImage().getScaledInstance((int) Math.ceil(imgIco.getIconWidth()/3*2), (int) Math.ceil(imgIco.getIconHeight()/3*2), Image.SCALE_DEFAULT));
+							System.out.println(imgIco.getDescription());
+							System.out.println("ICON WIDTH" + imgIco.getIconWidth());
+							img.setIcon(imgIco);
+							card = newCard;
+							humanFrame.pack();
+						}
+					}
+				catch (Exception e)
+				{
+						System.err.println("IndexOutOfBoundsException: " + e.getMessage());
+						System.out.println("null pointer --  cant compare cards for image");
+						waits(1000);
+					}
+				}
+			}};a.run();
+	}
+
 	@Override
    public boolean RequestCard(AI robot)//requests card from other player
 	{
